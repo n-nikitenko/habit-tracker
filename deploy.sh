ad56373b1bc711a20988d9e687bb731d64fb2534
nnikitenko <nnikitenko@aorti.ru>
2024-10-03 14:19:07 +0300

    cd using docker

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,1 @@
-/etc/poetry/bin/poetry shell
-/etc/poetry/bin/poetry  install --no-root
-/etc/poetry/bin/poetry run pip3 install gunicorn
-/etc/poetry/bin/poetry run python3 manage.py migrate
-/etc/poetry/bin/poetry run python3 manage.py collectstatic --no-input
-exit
+docker-compose --up build -d
