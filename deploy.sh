9c52ebf13f50fe6eed311734ded84e0a6fc52f21
nnikitenko <nnikitenko@aorti.ru>
2024-10-03 12:53:42 +0300

    fix cd

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-~/.local/share/pypoetry/venv/bin/poetry shell
-~/.local/share/pypoetry/venv/bin/poetry  install --no-root
+/home/nnikitenko/.local/share/pypoetry/venv/bin/poetry shell
+/home/nnikitenko/.local/share/pypoetry/venv/bin/poetry  install --no-root
 python3 manage.py migrate
 python3 manage.py collectstatic --no-input
 exit
